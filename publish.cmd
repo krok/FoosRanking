3ac3a80e2c7e065ec2a2319981ed9af76aec3ea9
Arnulf Krokeide <arnulf.krokeide@firmglobal.com>
2018-07-19 10:48:52 +0200

    "first commit"

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild FoosRanking.sln -p:DeployOnBuild=true -p:PublishProfile=foosranking-Web-Deploy.pubxml -p:Password=cS5hsdnaBfCuF4twxkrkho6TuBib1e7Z74bJloSWzPJZpG3kqwmsaAnaaDGE
+
